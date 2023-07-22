598e5a324694fbc760727afcb651e5133fa29236
kangkings <dlflq11@gmail.com>
2023-07-23 07:03:01 +0900

    배열 학습중

---
+++ src/com/exercise/course1/part2/ch06/TwoDimArrayTest.java
@@ -0,0 +1,8 @@
+package com.exercise.course1.part2.ch06;
+
+public class TwoDimArrayTest {
+    public static void main(String[] args) {
+        //행,열로 데이터 저장하기 (2차원 배열, 테이블구조, 행렬)
+        int
+    }
+}
