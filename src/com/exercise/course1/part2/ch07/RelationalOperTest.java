b8b8e3bcbbdf1990987a05e3e724c411c1910152
kangkings <dlflq11@gmail.com>
2023-07-24 11:40:27 +0900

    논리적인 사고력 키우기 학습중

---
+++ src/com/exercise/course1/part2/ch07/RelationalOperTest.java
@@ -0,0 +1,15 @@
+package com.exercise.course1.part2.ch07;
+
+public class RelationalOperTest {
+    public static void main(String[] args) {
+        //Q 관계연산자의 결과값을 쓰시오
+        int a = 4;
+        System.out.println(a>5);//0
+        System.out.println(a<5);//1
+        System.out.println(a == 6);//0
+        System.out.println(a!=4);//0
+        System.out.println(3>=a);//0
+        System.out.println(a<=(a+1));//1
+
+    }
+}
