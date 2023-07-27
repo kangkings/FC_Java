fd32f3d7c9a542a71374cfd03f9fbfb239be4c57
kangkings <dlflq11@gmail.com>
2023-07-27 14:31:54 +0900

    클래스로 객체 모델링하기 학습중

---
+++ src/com/exercise/course1/part3/ch01/PersonTest.java
@@ -0,0 +1,11 @@
+package com.exercise.course1.part3.ch01;
+
+public class PersonTest {
+    public static void main(String[] args) {
+        //Q 한 사람의 데이터를 저장할 변수를 선언하시오
+        //모델링 된 Person클래스를 이용하여 객체를 메모리에 생성하시오
+        //인스턴스를 만드는 과정은 다음과 같음
+        Person p;//같은 디렉토리에 있고 접근가능한 제어자로 선언된 클래스는 위치 명시나 import없이 사용할 수 있다
+        p = new Person();
+    }
+}
