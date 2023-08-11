f8f2e690a5d5ea2e3b33a0a55dcf0fa248bf6ecd
kangkings <dlflq11@gmail.com>
2023-08-11 16:51:54 +0900

    APIs 학습완료

---
+++ src/com/exercise/course2/part2/ch01/GsonFromAPI.java
@@ -0,0 +1,13 @@
+package com.exercise.course2.part2.ch01;
+
+import com.google.gson.Gson;
+
+public class GsonFromAPI {
+    public static void main(String[] args) {
+        String json = "{\"name\":\"홍길동\",\"age\":30}";//JSON -> Person
+        Gson g = new Gson();
+        Person p = g.fromJson(json,Person.class);
+        System.out.println(p.getAge());
+        System.out.println(p.getName());
+    }
+}
---
+++ src/com/exercise/course2/part2/ch01/GsonToAPI.java
@@ -0,0 +1,13 @@
+package com.exercise.course2.part2.ch01;
+
+import com.google.gson.Gson;
+
+public class GsonToAPI {
+    public static void main(String[] args) {
+        Person p = new Person("John", 20);
+        Gson g = new Gson();
+        String json = g.toJson(p);
+        System.out.println(json);
+
+    }
+}
